923c5d0a2da7fdb375ae6272e7d4838f658f0356
sophiachahine <schahine231@gmail.com>
2024-09-16 21:01:12 -0500

    accidently took off HERO

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<HERO />} />
         <Route path="/ admin-login" element={<ADMINLOGIN />} />
       </Routes>
+      <HERO />
       <Footer />
     </Router>
   );
