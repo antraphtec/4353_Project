f8e662fe3e23380221cddac54576f74317725c74
sophiachahine <schahine231@gmail.com>
2024-09-16 21:33:34 -0500

    draft login page added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./NavBar.css";
 
-const NavigationBar = () => {
+const NavigationBar = ({ handleSignInClick }) => {
   return (
     <nav className="navbar">
       <div className="logo">
@@ -9,19 +9,16 @@ const NavigationBar = () => {
         <span>CauseConnect</span>
       </div>
       <ul className="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/get-involved">Get Involved</a></li>
         <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/get-involved">Get Involved</a>
-        </li>
-        <li>
-          <a href="/signin" className="sign-in-btn">
+          <button onClick={handleSignInClick} className="sign-in-btn">
             Sign In
-          </a>
+          </button>
+          {/* <a href="/signin" className="sign-in-btn">
+            Sign In
+          </a> */}
         </li>
         <li>
           <a href="/volunteer" className="volunteer-btn">
--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NAVBAR from "./components/NavBar";
 import HERO from "./components/hero";
 import Footer from "./components/footer";
 import ADMINLOGIN from "./components/AdminLogin";
+import './styles/LoginModal.css';
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleSignInClick = () => {
+    setShowLogin(true);
+  };
+
+  const closeLogin = () => {
+    setShowLogin(false);
+  };
+  
   return (
     <Router>
-      <NAVBAR />
-      <Routes>
-        <Route path="/" element={<HERO />} />
-        <Route path="/ admin-login" element={<ADMINLOGIN />} />
-      </Routes>
+        <NAVBAR handleSignInClick={handleSignInClick} />
       <HERO />
+      <Routes>
+        <Route path="/admin-login" element={<ADMINLOGIN />} />
+      </Routes>
+
+      {showLogin && (
+        <div className="login-modal">
+          <ADMINLOGIN />
+          <button onClick={closeLogin}>Close</button> 
+        </div>
+      )}
+
       <Footer />
     </Router>
   );
