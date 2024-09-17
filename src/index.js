6510451c842326ff3dbf23e60af5afbd80ef582a
sophiachahine <schahine231@gmail.com>
2024-09-16 21:00:01 -0500

    routes in index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NAVBAR from "./components/NavBar";
 import HERO from "./components/hero";
 import Footer from "./components/footer";
-import AdminLogin from "./components/AdminLogin";
+import ADMINLOGIN from "./components/AdminLogin";
 
 function App() {
   return (
     <Router>
       <NAVBAR />
-      <HERO />
+      <Routes>
+        <Route path="/" element={<HERO />} />
+        <Route path="/ admin-login" element={<ADMINLOGIN />} />
+      </Routes>
       <Footer />
     </Router>
   );
