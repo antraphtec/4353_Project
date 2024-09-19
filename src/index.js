09834b96896dfdaacadebd16d397b092cdb32e82
sophiachahine <schahine231@gmail.com>
2024-09-19 00:50:54 -0500

    idk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NAVBAR from "./components/NavBar";
 import HERO from "./components/hero";
 import Footer from "./components/footer";
@@ -13,8 +13,12 @@ function App() {
   return (
     <Router>
       <NAVBAR />
+      <HERO />
       <Routes> 
         <Route path="/" element={<HERO />} /> 
+        <Route path="/about" element={<div>About Page</div>} />
+        <Route path="/get-involved" element={<div>Get Involved Page</div>} />
+        <Route path="/volunteer" element={<div>Volunteer Page</div>} />
         <Route path="/profile" element={<ProfileManagement />} /> 
         <Route path="/matching" element={<VolunteerMatching />} />
         <Route path="/login" element={<Login />} /> 
